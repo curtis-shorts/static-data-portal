f008ab7c4002081abaee93afa3c64ccbb85290ff
curtis-shorts <curtis.shorts@queensu.ca>
2024-06-04 13:05:51 -0400

    side-by-side trial

--- pages/index.tsx
+++ pages/index.tsx
@@ -183,7 +183,7 @@ export default function Home() {
               <Box p={2}>
                 <Box p={2}>
                   <InputGroup>
-                    <InputLeftAddon>Source</InputLeftAddon>
+                    <InputLeftAddon>Pull it up!</InputLeftAddon>
                     <Input
                       value={source.display_name || source.name}
                     />
